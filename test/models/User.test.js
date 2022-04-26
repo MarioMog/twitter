93e18ae69c5f305612520dd335dedfb86f8ba6c0
MarioMog <mamg3108@gmail.com>
2022-04-26 16:08:54 -0500

    test: user object

---
+++ test/models/User.test.js
@@ -0,0 +1,20 @@
+const User = require('./../../app/models/User')
+
+describe('Unit test for User class', () => {
+  test('Create an User object', () => {
+    const user = new User(
+      1,
+      'carlogilmar',
+      'Carlo',
+      'Bio',
+      'dateCreated',
+      'lastUpdated'
+    )
+    expect(user.id).toBe(1)
+    expect(user.username).toBe('carlogilmar')
+    expect(user.name).toBe('Carlo')
+    expect(user.bio).toBe('Bio')
+    expect(user.dateCreated).toBeDefined()
+    expect(user.lastUpdated).toBeDefined()
+  })
+})
