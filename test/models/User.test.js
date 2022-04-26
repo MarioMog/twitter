0f403ed106a4722d084d8b50fc7ad32b26fdff65
MarioMog <mamg3108@gmail.com>
2022-04-26 16:12:59 -0500

    test: user object

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,14 +2,7 @@ const User = require('./../../app/models/User')
 
 describe('Unit test for User class', () => {
   test('Create an User object', () => {
-    const user = new User(
-      1,
-      'carlogilmar',
-      'Carlo',
-      'Bio',
-      'dateCreated',
-      'lastUpdated'
-    )
+    const user = new User(1, 'carlogilmar', 'Carlo', 'Bio')
     expect(user.id).toBe(1)
     expect(user.username).toBe('carlogilmar')
     expect(user.name).toBe('Carlo')
